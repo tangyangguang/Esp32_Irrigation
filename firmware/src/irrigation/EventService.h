94ca6607f86f5cfa8f2f98d95cf2ed81f4395f5b
tttabc123 <254030999@qq.com>
2026-06-11 09:26:19 +0800

    chore: add irrigation event service wrapper

--- firmware/src/irrigation/EventService.h
+++ firmware/src/irrigation/EventService.h
@@ -1,6 +1,14 @@
 #pragma once
 
+#include <stdint.h>
+
 namespace irrigation {
+
+enum class IrrigationEventLevel : uint8_t {
+    Info,
+    Warning,
+    Fault,
+};
 
 class EventService {
 public:
@@ -10,6 +18,19 @@ public:
     void handle();
 
     bool ready() const;
+    bool append(IrrigationEventLevel level,
+                const char* type,
+                const char* reason,
+                const char* object,
+                const char* text = nullptr,
+                uint16_t code = 0,
+                int32_t value1 = 0,
+                int32_t value2 = 0,
+                int32_t value3 = 0,
+                uint8_t valueMask = 0);
+    bool info(const char* type, const char* reason, const char* object, const char* text = nullptr);
+    bool warning(const char* reason, const char* object, const char* text = nullptr);
+    bool fault(const char* reason, const char* object, const char* text = nullptr);
 
 private:
     bool _ready = false;
