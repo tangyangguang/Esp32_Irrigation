b6232735609fbb396a1ec1aab2baca3d1f0e1a62
tttabc123 <254030999@qq.com>
2026-06-11 09:21:38 +0800

    chore: wire recommended hardware pins

--- firmware/src/irrigation/IrrigationHardware.h
+++ firmware/src/irrigation/IrrigationHardware.h
@@ -17,6 +17,15 @@ public:
     bool ready() const;
     const IrrigationPinMap& pinMap() const;
 
+    int8_t flowInputPin() const;
+    int8_t lowLevelInputPin() const;
+    int8_t valveOutputPin(uint8_t zoneId) const;
+    int8_t pumpStartOutputPin() const;
+
+    bool setValveOutput(uint8_t zoneId, bool enabled);
+    void setPumpStartOutput(bool enabled);
+    bool lowLevelActive() const;
+
 private:
     IrrigationPinMap _pinMap = kRecommendedPinMap;
     bool _ready = false;
